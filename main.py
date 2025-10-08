141aab2f5958cdccb4b75c1ed403e7ee024107e5
emreaskinsoftware <emreaskinoffical@mail.com>
2025-10-08 15:39:10 +0300

    Initial commit: Add project files

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+import shutil
+
+path ="C:/Users/muhammet/Desktop/Deneme"
+remove_ex = [".log",".tmp"]
+move_ex = {".docx":"Documents",".txt":"Documents",".png":"Pictures",".jpeg":"Pictures",
+           ".jpg":"Pictures",".pptx":"Powerpoint Files",".xlsx":"Excel Files"}
+processed_files_count = 0
+
+def moveFile(filePath,destination):
+    if not os.path.exists(destination):
+        os.mkdir(destination)
+    shutil.move(filePath,destination)
+  
+for file in os.listdir(path):
+    filePath = os.path.join(path,file)
+    fileName,file_ex = os.path.splitext(file)
+    
+    if file_ex in move_ex:
+        destination_folder_name = move_ex[file_ex] 
+        full_destination_path = os.path.join(path, destination_folder_name) 
+        moveFile(filePath, full_destination_path) 
+        processed_files_count += 1
+
+    elif file_ex in remove_ex:
+        try:
+            os.remove(os.path.join(path,file))
+            processed_files_count += 1
+        except Exception as e:
+            print(e)
+
+if processed_files_count == 0:
+    print("İşlem yapılacak herhangi bir dosya bulunamadı.")
+else:
+    print(f"Toplam {processed_files_count} adet dosya işlendi.")
+
